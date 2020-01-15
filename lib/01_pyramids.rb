fc944806a3b60d1652bd0d94b6765103e545574e
lifeae <romain.leporatti@laposte.net>
2020-01-15 18:50:07 +0100

    done

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,36 +1,23 @@
-def how_much_stairs
-  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+def ask_stairs_number
+  puts "Mets un nombre pour voir ma belle pyramide"
   stairs = gets.chomp.to_i
-  while stairs < 1 || stairs > 25
-    puts "Abuse-pas et choisis un nombre entre 1 et 25 s'il te plaît. Alors ... combien d'étages ?"
-    stairs = gets.chomp.to_i
-  end
-  puts "Voici la pyramide :"
   return stairs
-end
-  
-def half_pyramid(stairs)
-  i = 1
-  while i <= stairs
-    i.times
-      j = stairs - i
-      puts (" "*j + "#"*i) 
-      i += 1
-  end
 end
 
 def full_pyramid(stairs)
   i = 1
+  k = 0
   while i <= stairs
     i.times
-      puts "#"*i 
+      j = stairs - i
+      puts (" "*j + "#"*i + "#"*k)
       i += 1
+      k += 1
   end
 end
 
 def perform
-  stairs = how_much_stairs
-  half_pyramid(stairs)
+  stairs = ask_stairs_number
   full_pyramid(stairs)
 end
 
